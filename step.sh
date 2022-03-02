5fa034709a031a38a0995c7dff6c236c2725a9d8
rios <rios>
2022-03-02 13:51:02 +0100

    wip

--- step.sh
+++ step.sh
@@ -5,7 +5,7 @@ set -ex
 echo "Installing saucectl"
 #npm install saucectl@0.84.0
 #curl -L https://saucelabs.github.io/saucectl/install | bash
-sh -c 'curl -L https://saucelabs.github.io/saucectl/install | bash -s -- -b /usr/local/bin'
+sudo sh -c 'curl -L https://saucelabs.github.io/saucectl/install | bash -s -- -b /usr/local/bin'
 
 echo "Running saucectl"
 saucectl run --config $sauce_config_yml $saucectl_run_options 2>&1
